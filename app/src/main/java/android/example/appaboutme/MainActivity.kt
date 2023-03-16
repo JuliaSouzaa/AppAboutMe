b24ff42cd3b7d8012514afd3d3a379dc3c91dce2
Lucas Lemos <124939307+LukassLemos@users.noreply.github.com>
2023-03-16 07:55:51 -0300

    App About Me

---
+++ app/src/main/java/android/example/appaboutme/MainActivity.kt
@@ -0,0 +1,11 @@
+package android.example.appaboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
